a106d12b421ebb21003116236f50461c0b022205
Kris Napiontek <kris@danalto.com>
2019-07-23 09:48:31 +0100

    yarn build

---
+++ build/precache-manifest.16576737a6226dad5e4ce61ccad553e1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57fe69520ae5c65c91a2d6201008d4f",
+    "url": "/table/index.html"
+  },
+  {
+    "revision": "123a2a510c6b1b3a27ed",
+    "url": "/table/static/js/2.d67af5f5.chunk.js"
+  },
+  {
+    "revision": "7d014eae20f2076a04ba",
+    "url": "/table/static/js/main.0da30943.chunk.js"
+  },
+  {
+    "revision": "b3cbda236719799eb2e9",
+    "url": "/table/static/js/runtime~main.3858f217.js"
+  }
+]);
